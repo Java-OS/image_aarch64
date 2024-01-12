9c8819e49d892902c4c762afa1aaa64e846d939b
mah454 <mah454@gmail.com>
2024-01-12 05:22:54 +0330

    Fix build aarch64 scripts

--- run_emulator.sh
+++ run_emulator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-sudo qemu-system-aarch64 -accel tcg,thread=multi -m 2G -M virt -cpu max -drive format=raw,file=rootfs/build-system/disk.img -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd -nographic
+qemu-system-aarch64 -accel tcg,thread=multi -m 2G -M virt -cpu max -drive format=raw,file=disk.img -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd -nographic
